02ca9d45eda4526d66441a1c5ec7a56fe92b0904
Eisuke Esaki <eesaki@e1z2r6p5.42.us.org>
2019-04-26 20:40:51 -0700

    minor changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,41 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/14 15:17:05 by eesaki            #+#    #+#             */
-/*   Updated: 2019/04/14 15:17:05 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/04/26 18:35:52 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<test purpose
+#include <stdio.h>
+#include <fcntl.h>
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>test purpose
+
+int		get_next_line(const int fd, char **line)
+{
+
+}
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<test purpose
+int		main(void)
+{
+	int		fd;
+	char	*line;
+
+	if (fd = open("readme.txt", O_RDONLY) == - 1)
+	{
+		printf("file read error\n");
+		return (1);
+	}
+
+	get_next_line(fd, &line);
+	printf("line:%s\n", line);
+	get_next_line(fd, &line);
+	printf("line:%s\n", line);
+	get_next_line(fd, &line);
+	printf("line:%s\n", line);
+
+
+	return (0);
+}
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>test purpose
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,10 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/14 15:16:33 by eesaki            #+#    #+#             */
-/*   Updated: 2019/04/14 15:17:01 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/04/26 18:11:54 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#define BUFF_SIZE 3
+#include "libft/libft.h"
+#include <unistd.h>
