02320d72d3fdb75b6e7c8bc9e606d1b171d9e178
Eisuke Esaki <eesaki@e1z2r6p5.42.us.org>
2019-04-27 19:55:23 -0700

    added macro FD_MAX

--- get_next_line.h
+++ get_next_line.h
@@ -6,10 +6,12 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/14 15:16:33 by eesaki            #+#    #+#             */
-/*   Updated: 2019/04/26 18:11:54 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/04/27 18:49:15 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #define BUFF_SIZE 3
+#define FD_MAX 1024
+
 #include "libft/libft.h"
 #include <unistd.h>
