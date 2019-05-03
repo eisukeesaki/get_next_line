0585bbb90b6c764774ca8c7546bce8d06905dfce
Eisuke Esaki <eesaki@e1z2r6p5.42.us.org>
2019-05-02 20:03:52 -0700

    minor changes, added 42FC main

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/14 15:17:05 by eesaki            #+#    #+#             */
-/*   Updated: 2019/04/30 21:01:18 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/05/02 20:01:48 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,21 @@ int		get_line(char **str, char **line)
 
 	if (*str)
 	{
-		if (!(nl = ft_strchr(*str, '\n')))
+		if (!(nl = ft_strchr(*str, '\n'))) // no \n
 		{
 			*line = ft_strdup(*str);
-			*str = ft_strnew(0);
+			*str[0] = '\0';
 			return (1);
 		}
-		if (nl != *str)
+		if (nl != *str) // \n not at beginning
 		{
 			*line = ft_strndup(*str, (nl - *str));
 			*str = nl + 1;
 		}
-		else
+		else // \n at beginning
 		{
 			*line = ft_strnew(0);
+			// *line = NULL;
 			*str = nl + 1;
 			return (1);
 		}
@@ -78,7 +79,6 @@ int		get_next_line(const int fd, char **line)
 // {
 // 	int		fd;
 // 	char	*line = NULL;
-// 	// int		ret = 0;
 	
 // 	if (ac == 1)
 // 		fd = 0;
@@ -91,13 +91,42 @@ int		get_next_line(const int fd, char **line)
 // 	{
 // 		ft_putendl(line);
 // 		ft_strdel(&line);
-	
-// 		// ret = ;
-// 		// printf("line:%s\n", line);
-// 		// if (!ret)
-// 		// 	break;
 // 	}
-
+// 	close(fd);
+// 	while (1);
 // 	return (0);
 // }
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>test purpose
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<42FC
+int				main(void)
+{
+	char		*line = NULL;
+	int			fd;
+
+	fd = open("./input.txt", O_RDONLY);
+	printf("fd:%d\n", fd);
+	get_next_line(fd, &line);
+	printf("line:%s\n", line);
+	close(fd);
+	free(line);
+	// while (1);
+
+	fd = open("./input2.txt", O_RDONLY);
+	if (fd == -1)
+		puts("file open error");
+	printf("fd:%d\n", fd);
+	get_next_line(fd, &line);
+	printf("line:%s\n", line);
+	close(fd);
+	free(line);
+
+	// fd = open("./input3.txt", O_RDONLY);
+	// printf("fd:%d\n", fd);
+	// get_next_line(fd, &line);
+	// printf("line:%s\n", line);
+	// close(fd);
+
+	return (0);
+}
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>42FC
